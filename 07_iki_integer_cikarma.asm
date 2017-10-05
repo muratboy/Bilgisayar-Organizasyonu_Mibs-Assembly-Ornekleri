48f7e811bd6f10f9a4377da68510c1d1894f630f
muratboy <muratboy.comu@gmail.com>
2017-10-05 18:53:44 +0300

    Update 07_iki_integer_cikarma.asm

--- 07_iki_integer_cikarma.asm
+++ 07_iki_integer_cikarma.asm
@@ -8,5 +8,5 @@
 	sub $t2, $t0, $t1	# t2 = t0 - t1
 	
 	li   $v0, 1
-	move $a0, $t2	# a0 <- 0 + t2 (add  $a0, $zero, $t2)da yazilabilir
+	move $a0, $t2		# a0 <- 0 + t2 (add  $a0, $zero, $t2)da yazilabilir
 	syscall
