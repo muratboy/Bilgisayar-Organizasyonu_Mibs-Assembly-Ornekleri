3d86b26f307834f6ffbba45a47245edb1d287da9
muratboy <muratboy.comu@gmail.com>
2017-10-05 18:51:50 +0300

    Update 01_string_yazdirma.asm

--- 01_string_yazdirma.asm
+++ 01_string_yazdirma.asm
@@ -4,4 +4,4 @@
 .text
 	li	$v0,	4	#string yazdirmak icin $v0 4 yapilmali
 	la	$a0,	Mesaj	#Mesajin adresini $a0'a at
-	syscall	#ekrana yazma islemi icin isletim sistemini cagir(sistem cagrisi)
+	syscall			#ekrana yazma islemi icin isletim sistemini cagir(sistem cagrisi)
